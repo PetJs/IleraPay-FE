c73ef76d51085f54d7dbc57e5906c9f753c017ec
PetJs <peterfagoroye76@gmail.com>
2025-07-22 02:19:21 +0100

    Adds Sign Up page

---
+++ src/pages/auth/singUoOage.tsx
@@ -0,0 +1,12 @@
+
+
+export const SignUpPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6">Sign Up</h2>
+        {/* Sign Up Form Goes Here */}
+      </div>
+    </div>
+  );
+}
