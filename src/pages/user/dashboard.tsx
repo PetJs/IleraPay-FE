73c8dfc310f0a16be338777b4ec585a2f4752185
PetJs <peterfagoroye76@gmail.com>
2025-07-27 19:40:50 +0100

    Adds Dashboard

--- src/pages/user/dashboard.tsx
+++ src/pages/user/dashboard.tsx
@@ -1,7 +1,19 @@
+import CardComponent from "@/components/card/card";
+import { WalletMinimal } from "lucide-react";
+import { ArrowRightCircle } from "lucide-react";
+
+const handleCardClick = () => {
+  console.log("Card clicked");
+}
+
 const Dashboard = () => (
-  <div>
-    <h1>Welcome to your Dashboard!</h1>
-    <p>You are signed in.</p>
+  <div className="p-4">
+    <div className="grid grid-cols-2 gap-4 ">
+      <CardComponent title="Wallet" description="$2000" bgColor="bg-[#5D8AA8]" icon={<WalletMinimal/>} openIcon={<ArrowRightCircle/>}/>
+      <CardComponent onClick={handleCardClick} description="$2000" bgColor="bg-[#FF4F00]"/>
+      <CardComponent title="Wallet" description="$2000" bgColor="bg-[#AB274F]"/>
+      <CardComponent title="Wallet" description="$2000" bgColor="bg-[#C0C0C0]"/>
+    </div>
   </div>
 );
 
