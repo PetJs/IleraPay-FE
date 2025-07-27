e983cdda1d61859bb931e806a4ffdfead2bb3cc3
PetJs <peterfagoroye76@gmail.com>
2025-07-27 19:41:03 +0100

    Fixs Minor changes

--- src/layouts/dashboardLayout.tsx
+++ src/layouts/dashboardLayout.tsx
@@ -1,10 +1,14 @@
 import {User} from "lucide-react";
 import { Outlet } from "react-router-dom";
 
+const date = new Date()
+const hours = date.getHours();
+const greeting = hours < 12 ? "Morning" : hours < 18 ? "Afternoon" : "Evening";
+
 const DashboardLayout = () => (
   <div className="min-h-screen flex flex-col bg-gray-100">
     <header className="bg-white flex justify-between items-center p-2">
-      <h1 className="text-xl font-bold">Good Evening, Jared</h1>
+      <h1 className="text-xl font-bold">Good {greeting}, Jared</h1>
       <div className="flex justify-between gap-2 items-center">
         <p>Hi</p>
         <User className="w-8 h-8 text-gray-700" fill="blue" stroke="" />
